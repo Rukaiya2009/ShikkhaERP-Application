3c1f91564836e5d61ca598b27c8edd13a44f6577
Farhanahoque251 <farhanahoque251@gmail.com>
2026-06-06 01:45:56 +0600

    test
    test api

---
+++ shikkhaerp-backend/src/main/java/com/shikkhaerp/ShikkhaErpApplication.java
@@ -0,0 +1,12 @@
+package com.shikkhaerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShikkhaErpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShikkhaErpApplication.class, args);
+        System.out.println("🚀 ShikkhaERP Started!");
+    }
+}
